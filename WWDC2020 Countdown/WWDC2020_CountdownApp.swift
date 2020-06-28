6f77bfc0071ba581d73889280b433878f34fd8bf
Igor Malyarov <igor1309@gmail.com>
2020-06-28 23:30:44 +0300

    Initial Commit

---
+++ WWDC2020 Countdown/WWDC2020_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2020_CountdownApp.swift
+//  WWDC2020 Countdown
+//
+//  Created by temp on 28.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2020_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
